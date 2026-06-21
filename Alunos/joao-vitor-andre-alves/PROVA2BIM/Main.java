97e5aaabe7870127fc74b205ed1f0e4fbb718eab
João Vitor Alves <jvaa7476@gmail.com>
2026-06-21 19:21:43 -0300

    feature: adiciona tela principal e a main

---
+++ Alunos/joao-vitor-andre-alves/PROVA2BIM/Main.java
@@ -0,0 +1,11 @@
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        // SwingUtilities.invokeLater = iniciar tela Swing
+        SwingUtilities.invokeLater(() -> {
+            TelaPrincipal tela = new TelaPrincipal();
+            tela.setVisible(true);
+        });
+    }
+}
---
+++ Alunos/joao-vitor-andre-alves/PROVA2BIM/TelaPrincipal.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class TelaPrincipal extends JFrame {
+    
+    public TelaPrincipal() {
+        setTitle("Minhas series");
+        setSize(600, 400);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+    }
+}
